43a28b1f5701693972c138486a334d91f12d647c
Vanja Parezanovic <vanja.parezanoviccc@gmail.com>
2025-09-27 16:50:21 +0200

    Stranica o nama i moji bonusi

--- hr-react/src/pages/AboutUs.jsx
+++ hr-react/src/pages/AboutUs.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Title from "../components/Title";
 
 const AboutUs = () => {
     return (
         <div>
-            About us
+            <Title title="About us"/>
+            <p className="mt-4">
+                Axe HR System is a cutting-edge human resources management platform designed to streamline and enhance the way organizations manage their workforce. Our system offers a comprehensive suite of tools and features that cater to the diverse needs of HR professionals, employees, and management teams.
+            </p>
+            <p>
+                With Axe HR System, organizations can efficiently handle various HR functions, including employee onboarding, performance management, payroll processing, benefits administration, and compliance tracking. Our user-friendly interface ensures that both HR personnel and employees can easily navigate the system and access the information they need.
+            </p>
+            <p>
+                One of the key highlights of Axe HR System is its robust analytics and reporting capabilities. The platform provides real-time insights into workforce trends, enabling organizations to make data-driven decisions that improve employee engagement, productivity, and retention.
+            </p>
+            <p>
+                Additionally, Axe HR System prioritizes security and data privacy, ensuring that sensitive employee information is protected through advanced encryption and access controls. Our commitment to compliance with industry regulations further reinforces our dedication to safeguarding organizational data.
+            </p>
+            <p>
+                Whether you're a small business or a large enterprise, Axe HR System is designed to scale with your organization's growth. Our customizable solutions allow you to tailor the platform to meet your specific HR needs, making it a versatile choice for businesses across various industries.
+            </p>
         </div>
     );
 };
--- hr-react/src/pages/MyBonuses.jsx
+++ hr-react/src/pages/MyBonuses.jsx
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import instance from "../app-logic/instance";
+import {toast} from "react-toastify";
+import Title from "../components/Title";
+import {Col, Row, Table} from "react-bootstrap";
 
 const MyBonuses = () => {
+
+    const token = sessionStorage.getItem('token');
+    const user = JSON.parse(sessionStorage.getItem('user'));
+
+    if (!token) {
+        window.location.href = '/';
+    }
+
+    const [bonuses, setBonuses] = React.useState([]);
+
+    useEffect(() => {
+        instance.get('/users/' + user.id +'/bonuses')
+            .then(response => {
+                const fetchedData = response.data;
+                if (fetchedData.success === true) {
+                    setBonuses(fetchedData.data);
+                } else {
+                    toast.info("No bonuses found.");
+                }
+            }).catch(error => {
+            console.error("Error fetching bonuses data:", error);
+            toast.error("Error fetching bonuses data.");
+        })
+    }, []);
+
     return (
-        <div>
-            MyBonuses
-        </div>
+        <>
+            <Title title="My Bonuses"/>
+            <Row>
+                <Col md={12}>
+                    <Table hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Amount</th>
+                            <th>Date Awarded</th>
+                            <th>Note</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {bonuses.length > 0 ? bonuses.map((bonus, index) => (
+                            <tr key={bonus.id}>
+                                <td>{index + 1}</td>
+                                <td>${bonus.amount}</td>
+                                <td>{new Date(bonus.date_awarded).toLocaleDateString()}</td>
+                                <td>{bonus.note}</td>
+                            </tr>
+                        )) : (
+                            <tr>
+                                <td colSpan="4" className="text-center">No bonuses found.</td>
+                            </tr>
+                        )
+                        }
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </>
     );
 };
 
